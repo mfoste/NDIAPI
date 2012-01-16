80e3bf7964d67d25619747ab0dca7bdb6bda13c9
Andrew Wiles <awiles@awileslnx-ubuntu.(none)>
2012-01-16 13:52:17 -0500

    Added a .gitignore file and fixed the Sleep calls for unix in NDITrack.exe

--- Examples/NDITrack/NDITrack.cxx
+++ Examples/NDITrack/NDITrack.cxx
@@ -1,8 +1,21 @@
-#include <windows.h>
 #include <time.h>
 #include <string>
 #include <iostream>
 #include <fstream>
+
+/* =========== windows includes */
+#if defined(WIN32) || defined(_WIN32)
+#include <windows.h>
+
+/* =========== unix includes */
+#elif defined(unix) || defined(__unix__) || defined(__APPLE__)
+#include <unistd.h>
+
+/* =========== mac includes */
+#elif defined(macintosh)
+//TODO: add appropriate headers for sleep here.
+#endif
+
 
 #include <vtkSmartPointer.h>
 #include <vtkCommand.h>
@@ -46,19 +59,19 @@ public:
       double pos[3], quat[4];
       tool->GetTransform()->GetOrientationWXYZ(quat);
       tool->GetTransform()->GetPosition(pos);
-      
+
       std::cout.setf(std::ios_base::fixed);
-      std::cout << this->m_nXfrms << ". " << tool->GetToolPartNumber() 
-        << " updated (t = " << tool->GetTimeStamp() << ").  Effective frequency: " 
-        << this->m_effectiveFrequency << ". Xfrm: " 
+      std::cout << this->m_nXfrms << ". " << tool->GetToolPartNumber()
+        << " updated (t = " << tool->GetTimeStamp() << ").  Effective frequency: "
+        << this->m_effectiveFrequency << ". Xfrm: "
         << quat[0] << "," << quat[1] << "," << quat[2] << "," << quat[3] << ","
         << pos[0] << "," << pos[1] << "," << pos[2] << std::endl;
       }
       else
       {
         std::cout.setf(std::ios_base::fixed);
-        std::cout << this->m_nXfrms << ". " << tool->GetToolPartNumber() 
-        << " updated (t = " << tool->GetTimeStamp() << ").  Effective frequency: " 
+        std::cout << this->m_nXfrms << ". " << tool->GetToolPartNumber()
+        << " updated (t = " << tool->GetTimeStamp() << ").  Effective frequency: "
         << this->m_effectiveFrequency << ". Xfrm: Missing." << std::endl;
       }
     }
@@ -72,6 +85,8 @@ private:
 
 int main( int argc, char *argv[] )
 {
+  int milliseconds = int(25/2);
+
   vtkNDITracker* tracker = vtkNDITracker::New();
   vtkSmartPointer<vtkTransformUpdateCallback> cb = vtkSmartPointer<vtkTransformUpdateCallback>::New();
   double pos[3];
@@ -107,23 +122,44 @@ int main( int argc, char *argv[] )
     tElapsed = ((double)(curr - start)) / CLOCKS_PER_SEC;
     if( tracker->GetTool(0)->IsMissing() )
     {
-      outFile << i << " Tool is missing.(t=" << tElapsed << "): Timestamp " 
-        << tracker->GetTool(0)->GetTimeStamp() << ", Last Update Time: " 
-        << tracker->GetUpdateTimeStamp() << std::endl;
+      outFile << i << " Tool is missing.(t=" << tElapsed << "): Timestamp "
+              << tracker->GetTool(0)->GetTimeStamp() << ", Last Update Time: "
+              << tracker->GetUpdateTimeStamp() << std::endl;
     }
     else
     {
       tracker->GetTool(0)->GetTransform()->GetOrientationWXYZ(quat);
       tracker->GetTool(0)->GetTransform()->GetPosition(pos);
       outFile << i << " Xfrm (t=" << tElapsed << "): Timestamp " << tracker->GetTool(0)->GetTimeStamp() << ","
-        << quat[0] << "," << quat[1] << "," << quat[2] << "," << quat[3] << ","
-        << pos[0] << "," << pos[1] << "," << pos[2] << ", Last Update Time: " 
-        << tracker->GetUpdateTimeStamp() << std::endl;
-      Sleep(25/2);
+              << quat[0] << "," << quat[1] << "," << quat[2] << "," << quat[3] << ","
+              << pos[0] << "," << pos[1] << "," << pos[2] << ", Last Update Time: "
+              << tracker->GetUpdateTimeStamp() << std::endl;
+
+#if defined(WIN32) || defined(_WIND32)
+      Sleep(milliseconds);
+#elif defined(unix) || defined(__unix__) || defined(__APPLE__)
+#ifdef USE_NANOSLEEP
+      struct timespec sleep_time, dummy;
+      sleep_time.tv_sec = milliseconds/1000;
+      sleep_time.tv_nsec = (milliseconds - sleep_time.tv_sec*1000)*1000000;
+      nanosleep(&sleep_time,&dummy);
+#else /* use usleep instead */
+      /* some unices like IRIX can't usleep for more than 1 second,
+     so break usleep into 500 millisecond chunks */
+      while (milliseconds > 500) {
+        usleep(500000);
+        milliseconds -= 500;
+      }
+      usleep(milliseconds*1000);
+#endif
+#elif defined(macintosh)
+      //TODO: figure this out.
+#endif
+
     }
   }
   outFile.close();
-  
+
   tracker->StopTracking();
   std::cout << "Tracking stopped." << std::endl;
   std::cout << "Internal Update Rate: " << tracker->GetInternalUpdateRate() << std::endl;
